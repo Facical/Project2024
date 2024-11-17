d68a595be840569a8f572c29c11b0e95a75b0fef
zx462 <rladuwns1917@naver.com>
2024-11-17 23:07:32 +0900

    형준이형 ㅎㅇ

--- khj/src/KYJ.java
+++ khj/src/KYJ.java
@@ -2,6 +2,6 @@ public class KYJ {
     public static void main(String[] args) {
         System.out.println("Test1");
         System.out.println("KYJ TEST1111");
-        
+        System.out.println("형준이형 나 여준이야 ㅎㅎ");
     }
 }
